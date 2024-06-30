006d3653897f89dcfd4da4e346a38989bcb8457c
vodish <vodish@yandex.ru>
2024-06-30 17:19:14 +0300

    upd

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import express, { Request, Response } from 'express';
+const cors = require('cors')
+
+const { PORT = 3100 } = process.env;
+
+
+const app = express();
+app.use(cors({
+  origin: true,
+  credentials: true,
+}))
+
+app.listen(PORT, () => console.log(`Listen port http://localhost:${PORT}`))
+
+
+app.get('/test', (req: Request, res: Response) => {
+  const now = Date.now()
+  res.cookie('user_token', now, { httpOnly: true });
+  res.json({ test: 'test ' + now })
+});
