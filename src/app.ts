6d377aed4b8e6ecae5598eb49dbfc42546d7f98f
pkarasev <p.karasev@psw.ru>
2024-07-01 14:20:52 +0300

    upd

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
-import express, { Request, Response } from 'express';
-const cors = require('cors')
+import express, { ErrorRequestHandler, NextFunction, Request, Response } from 'express';
+import multer from 'multer'
 
 const { PORT = 3100 } = process.env;
+const cors = require('cors')
+// const upload = multer({dest: '/upload'})
+const upload = multer()
 
 
 const app = express();
@@ -18,3 +21,17 @@ app.get('/setcookie', (req: Request, res: Response) => {
   res.cookie('user_token', now, { httpOnly: true });
   res.json({ test: 'test ' + now })
 });
+
+app.post('/formdata', upload.none(), (req: Request, res: Response) => {
+  
+  // res.json({body: req.body, file: req.file})
+  res.json({body: req.body})
+});
+
+
+
+app.use( (err: any, req: Request, res: Response, next: NextFunction) => {
+  const { statusCode = 500, message } = err;
+  
+  res.status(statusCode).send(err)
+})
